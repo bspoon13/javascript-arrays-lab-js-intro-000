7bb7ee5f66b5b29603a1ddc21ef10b2f10eb4765
Billy Witherspoon <wwither@gmail.com>
2018-10-29 22:57:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 var kittens = ["Milo", "Otis", "Garfield"];
 
-function destructivelyAppendKitten(name) {
+function kittenDestruction(name) {
   var destructivelyAppendKitten = kittens.push(name);
+  
+}
+
+function destructivelyPrependKitten(name) {
   var destructivelyPrependKitten = kittens.unshift(name);
 }
 
